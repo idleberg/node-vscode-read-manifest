f905c27455d89f6cd3c01636438e631642c637be
floatwork_jan <jan@floatwork.at>
2019-05-28 13:24:12 +0200

    add support for current package

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 'use strict';
 
 const { extensions } = require('vscode');
+const findUp = require('find-up');
 const { promisify } = require('util');
 const { readFile, readFileSync } = require('fs');
 const { resolve } = require('path');
 
 const readFileAsync = promisify(readFile);
 
-module.exports = extensionID => {
-    const extensionPath = extensions.getExtension(extensionID).extensionPath;
-    const filePath = resolve(extensionPath, 'package.json');
+module.exports = (extensionID = false) => {
+    let filePath;
+
+    if (extensionID) {
+        const extensionPath = extensions.getExtension(extensionID).extensionPath;
+        filePath = resolve(extensionPath, 'package.json');
+    } else {
+        filePath = findUp('package.json');
+    }
 
     return readFileAsync(filePath, 'utf8').then(file => JSON.parse(file));
 };
 
-module.exports.sync = extensionID => {
-    const extensionPath = extensions.getExtension(extensionID).extensionPath;
-    const filePath = resolve(extensionPath, 'package.json');
+module.exports.sync = (extensionID = false) => {
+    let filePath;
+
+    if (extensionID) {
+        const extensionPath = extensions.getExtension(extensionID).extensionPath;
+        filePath = resolve(extensionPath, 'package.json');
+    } else {
+        filePath = findUp('package.json');
+    }
     const file = readFileSync(filePath, 'utf8');
 
     return JSON.parse(file);
